da48e88a8ae7df19c8ed5618de03789baa791313
anthonymnf <anthonymnf30@gmail.com>
2024-01-04 15:42:47 -0300

    firstCommit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Home from "./components/Home";
+import Products from "./components/Products";
+
+function App() {
+  return (
+    <>
+      <Home />
+      <Products />
+    </>
+  );
+}
+
+export default App;
