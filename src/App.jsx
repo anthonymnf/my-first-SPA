150579554f0a9a724432cec12c62ae9c29c8e125
anthonymnf <anthonymnf30@gmail.com>
2024-01-04 15:55:49 -0300

    add routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,8 @@
-import Home from "./components/Home";
-import Products from "./components/Products";
+import { RouterProvider } from "react-router-dom";
+import router from "./router";
 
 function App() {
-  return (
-    <>
-      <Home />
-      <Products />
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
