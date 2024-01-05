7a9f020f60a76d888ef3b85029e28a6c4fe94672
anthonymnf <anthonymnf30@gmail.com>
2024-01-05 09:59:50 -0300

    stylização da paginas de produtos

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import products from "../database.json";
+import "./styles/products.css";
 export default function Products() {
   return (
     <section>
@@ -7,14 +8,16 @@ export default function Products() {
       <p>Confira todas as nossas ofertas.</p>
       <section className="products">
         {products.map((product) => (
-          <div key={product.id}>
+          <div className="container" key={product.id}>
             <h4>{product.name}</h4>
             <img src={product.img} alt="" />
             <p>R$ {product.price}</p>
-            <Link to={`/products/${product.id}`}>
-              <button>Ver</button>
-            </Link>
-            <button>Comprar</button>
+            <div className="containerBtn">
+              <Link to={`/products/${product.id}`}>
+                <button>Ver</button>
+              </Link>
+              <button>Comprar</button>
+            </div>
           </div>
         ))}
       </section>
